e32d75293d2c194064fdfbda7481ccfcd6f3cb88
unanchoi <unanc2@gmail.com>
2023-12-02 12:02:36 +0900

    Test code 추가

--- src/main/java/com/example/seminar/entity/PostEntity.java
+++ src/main/java/com/example/seminar/entity/PostEntity.java
@@ -2,14 +2,20 @@ package com.example.seminar.entity;
 
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.AccessLevel;
 import lombok.Builder;
+import lombok.NoArgsConstructor;
 
 @Table(name = "post")
 @Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class PostEntity {
 
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @NotNull
     private String title;
     private String content;
 
